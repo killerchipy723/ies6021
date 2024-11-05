de6c5271617bd735234e5b78fc998a01be1829cb
killerchipy723 <diegoeduardo66@gmail.com>
2024-11-05 18:36:54 -0300

    okis

--- public/db.js
+++ public/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 // Crear la conexión a la base de datos con soporte de promesas
 const connection = mysql.createConnection({
     host: 'localhost',      // Cambia esto si tu base de datos está en otro servidor
-    user: 'root',           // Tu usuario de MySQL
-    password: '',           // Tu contraseña de MySQL
-    database: 'inst6021'    // Nombre de la base de datos
+    user: 'c2710325_ies6021',           // Tu usuario de MySQL
+    password: 'peTUgu87ga',           // Tu contraseña de MySQL
+    database: 'c2710325_ies6021'    // Nombre de la base de datos
 }).promise();               // Activar el modo de promesas
 
 // Exportar la conexión
