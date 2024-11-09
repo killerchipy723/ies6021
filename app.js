45d869623516579cf30e1c0e748d28eceeb13280
Diego Alderete <141648675+killerchipy723@users.noreply.github.com>
2024-11-09 13:58:54 -0300

    Update app.js
    great

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const generarConstanciaPDF = require('./constanciaPDF'); // Ajusta la ruta segú
 
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
  
 // Middleware para servir archivos estáticos y parsear JSON
 app.use(express.static(path.join(__dirname, 'public')));
