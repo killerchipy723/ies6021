6a0114c8065a93555a2295356a3cb1f69eb6d07e
killerchipy723 <diegoeduardo66@gmail.com>
2024-11-11 20:21:29 -0300

    prueba sql

--- public/db.js
+++ public/db.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2');
 
-// Crear la conexión a la base de datos con soporte de promesas
-const connection = mysql.createConnection({
-    host: 'localhost',      // Cambia esto si tu base de datos está en otro servidor
-    user: 'c2710325_killer',           // Tu usuario de MySQL
-    password: 'SistemaIES6021',           // Tu contraseña de MySQL
-    database: 'c2710325_sistema'    // Nombre de la base de datos
-}).promise();               // Activar el modo de promesas
 
-// Exportar la conexión
+const connection = mysql.createConnection({
+    host: 'localhost',      
+    user: 'c2710325_killer',           
+    password: 'SistemaIES6021',           
+    database: 'c2710325_ies6021'    
+}).promise();               
+
+
 module.exports = connection;
