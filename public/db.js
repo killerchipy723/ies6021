33b25bd9bca2a60e7d5356b82ae45bed6bf99442
killerchipy723 <diegoeduardo66@gmail.com>
2024-11-11 19:44:26 -0300

    pruebaDB

--- public/db.js
+++ public/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Crear la conexión a la base de datos con soporte de promesas
 const connection = mysql.createConnection({
-    host: 'sql10.freesqldatabase.com',      // Cambia esto si tu base de datos está en otro servidor
-    user: 'sql10742951',           // Tu usuario de MySQL
-    password: 'xL382AvCjK',           // Tu contraseña de MySQL
-    database: 'sql10742951'    // Nombre de la base de datos
+    host: 'localhost',      // Cambia esto si tu base de datos está en otro servidor
+    user: 'c2710325_killer',           // Tu usuario de MySQL
+    password: 'SistemaIES6021',           // Tu contraseña de MySQL
+    database: 'c2710325_sistema'    // Nombre de la base de datos
 }).promise();               // Activar el modo de promesas
 
 // Exportar la conexión
