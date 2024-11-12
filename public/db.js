05015925657ae0328c0a24b993e8dc0835c8ada6
killerchipy723 <diegoeduardo66@gmail.com>
2024-11-11 23:41:22 -0300

    ca

--- public/db.js
+++ public/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',      
     user: 'c2710325_killer',           
-    password: 'password',           
+    password: 'SistemaIES6021',           
     database: 'c2710325_sistema'    
 }).promise();               
 
