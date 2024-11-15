f2d0ba31edd79d84b8f8cbcec22724a81918b31a
killerchipy723 <diegoeduardo66@gmail.com>
2024-11-15 08:10:06 -0300

    ok estilos

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const MySQLStore = require('express-mysql-session')(session);
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const mysql = require('mysql2/promise');
-const fs = require('fs');
-const pdf = require('pdfkit');
-const os = require('os'); 
-const generarConstanciaPDF = require('./constanciaPDF'); // Ajusta la ruta según sea necesario
+
+
 const app = express();
 const port = 3000;
  
@@ -17,8 +15,6 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
 
 
 
@@ -71,7 +67,6 @@ app.post('/login', async (req, res) => {
             };
 
             const nombreCompleto = `${user[0].apellidos}, ${user[0].nombres}`;
-            const idalu = user[0].idalumno;
             return res.status(200).json({ success: true, nombreCompleto });
         } else {
             return res.status(401).json({ success: false, message: 'DNI o contraseña incorrectos.' });
@@ -94,7 +89,7 @@ app.get('/datos-alumno',  (req, res) => {
             } else {
                 res.status(404).json({ error: 'Alumno no encontrado' });
             }
-        }) 
+        })
         .catch(err => {
             console.error('Error al obtener datos del alumno:', err);
             res.status(500).json({ error: 'Error en el servidor' });
@@ -159,7 +154,7 @@ app.post('/send-reset-email', async (req, res) => {
         if (!users || users.length === 0) {
             return res.status(404).json({ message: 'No se encontró un usuario con ese DNI y correo electrónico.' });
         }
- 
+
         // Si existe el usuario, seleccionamos el primer elemento
         const user = users[0];
 
@@ -170,7 +165,7 @@ app.post('/send-reset-email', async (req, res) => {
 
         // Configurar el mensaje para el correo electrónico
         const message = {
-            text: `Aquí está el enlace para restablecer su contraseña: http://ies6021jcdavalos.online/reset-password?token=${token}`,
+            text: `Aquí está el enlace para restablecer su contraseña: http://localhost:3000/reset-password?token=${token}`,
             from: 'ies6.021jc@gmail.com',
             to: email,
             subject: 'Restablecimiento de Contraseña'
@@ -230,7 +225,7 @@ app.post('/registro', async (req, res) => {
 
     if (!dni1 || !correo) {
         return res.status(400).json({ message: 'DNI y correo son obligatorios.' });
-    }
+    } 
 
     if (clave1 !== clave2) {
         return res.status(400).json({ message: 'Las contraseñas no coinciden.' });
@@ -257,215 +252,6 @@ app.post('/registro', async (req, res) => {
     }
 });
 
-
-// TABLA INSCRIPCIONES
-
-app.get('/inscripciones', async (req, res) => {
-    if (!req.session.user || !req.session.user.dni) {
-        return res.status(401).json({ message: 'Usuario no autenticado' });
-    }
-
-    const idalu = app.post('/login', async (req, res) => {
-    const { dni, password } = req.body;
-
-    try {
-        const [user] = await db.query(
-            'SELECT * FROM alumno WHERE (dni = ? OR correo = ?) AND clave = ?',
-            [dni, dni, password]
-        );
-
-        if (user.length > 0) {
-            req.session.user = {
-                idalumno: user[0].idalumno,
-                dni: user[0].dni,
-                correo: user[0].correo,
-                nombres: user[0].nombres,
-                apellidos: user[0].apellidos
-            };
-
-            const nombreCompleto = `${user[0].apellidos}, ${user[0].nombres}`;
-            const idalu = user[0].idalumno;
-            return res.status(200).json({ success: true, nombreCompleto });
-        } else {
-            return res.status(401).json({ success: false, message: 'DNI o contraseña incorrectos.' });
-        }
-    } catch (error) {
-        console.error('Error en la autenticación:', error);
-        return res.status(500).json({ message: 'Error en la autenticación' });
-    }
-}); 
-
-    const dni = req.session.user.dni;
-    console.log("DNI recibido en el backend:", dni); // Verifica el DNI
-
-    try {
-        const [inscripciones] = await db.query(
-            `SELECT i.idinscripcion, CONCAT(a.apellidos, ', ', a.nombres) AS Alumno, 
-                    a.dni, c.nombre AS Carrera, 
-                    DATE_FORMAT(i.fecha, '%d-%m-%Y %H:%i:%s') AS fecha
-             FROM preinscripcion i
-             JOIN alumno a ON a.idalumno = i.idalumno
-             JOIN carreras c ON c.idcarrera = i.idcarrera
-             WHERE a.dni = ? AND i.estado = 'Activo'`,
-            [dni]
-        );
-
-        console.log("Resultado de la consulta:", inscripciones); // Verifica el resultado de la consulta
-
-        if (inscripciones.length > 0) {
-            return res.status(200).json({ success: true, inscripciones });
-        } else {
-            return res.status(200).json({ success: false, message: 'No se encontraron inscripciones para este usuario.' });
-        }
-    } catch (error) {
-        console.error('Error al obtener inscripciones:', error);
-        return res.status(500).json({ message: 'Error al obtener inscripciones' });
-    }
-});
-
-
-/// DAR BAJA A UNA INSCRIPCION DE CARRERA
-
-app.put('/inscripciones/baja/:idinscripcion', async (req, res) => {
-    const { idinscripcion } = req.params;
-
-    try {
-        // Actualizar el estado de la inscripción a "Baja"
-        const [result] = await db.query(
-            `UPDATE preinscripcion SET estado = 'Baja' WHERE idinscripcion = ?`,
-            [idinscripcion]
-        );
-
-        if (result.affectedRows > 0) {
-            res.status(200).json({ success: true, message: 'Inscripción dada de baja correctamente.' });
-        } else {
-            res.status(404).json({ success: false, message: 'Inscripción no encontrada.' });
-        }
-    } catch (error) {
-        console.error('Error al dar de baja la inscripción:', error);
-        res.status(500).json({ success: false, message: 'Error al dar de baja la inscripción' });
-    }
-});
-
-//VERIFICAR SI EL ALUMNO ESTA INSCRIPTO CON INSCRIPCION ACTIVA
-
-;
-
-
-app.post('/inscripciones/nueva', async (req, res) => {
-    const { idCarrera } = req.body;
-    const idAlumno = req.session.user?.idalumno;
-
-    // Verificar que idAlumno y idCarrera sean válidos
-    if (!idAlumno || !idCarrera) {
-        return res.status(400).json({ success: false, message: 'Datos incompletos' });
-    }
-
-    // Obtener la fecha y hora actual del sistema y restar 3 horas
-    const fechaActual = new Date();
-    fechaActual.setHours(fechaActual.getHours() - 3); // Restar 3 horas
-    const fechaFormateada = fechaActual.toISOString().slice(0, 19).replace('T', ' ');
-
-    try {
-        // Consultar si ya existe una inscripción activa para el alumno
-        const [existingInscription] = await db.query(
-            'SELECT estado FROM preinscripcion WHERE idalumno = ? AND estado = "Activo"',
-            [idAlumno]
-        );
-
-        // Verificar si hay una inscripción activa
-        if (existingInscription && existingInscription.length > 0) {
-            return res.status(400).json({
-                success: false,
-                message: 'Ya tienes una inscripción activa. No puedes inscribirte en dos carreras al mismo tiempo'
-            });
-        }
-
-        // Realizar la inscripción en la base de datos
-        await db.query(
-            'INSERT INTO preinscripcion (idalumno, idcarrera, fecha, estado) VALUES (?, ?, ?, ?)',
-            [idAlumno, idCarrera, fechaFormateada, 'Activo']
-        );
-
-        // Consultar los datos del alumno y la carrera para generar el PDF
-        const [inscripciones] = await db.query(
-            `SELECT i.idinscripcion, CONCAT(a.apellidos, ', ', a.nombres) AS Alumno, 
-                    a.dni, c.nombre AS Carrera, 
-                    DATE_FORMAT(i.fecha, '%d-%m-%Y %H:%i:%s') AS fecha
-             FROM preinscripcion i
-             JOIN alumno a ON a.idalumno = i.idalumno
-             JOIN carreras c ON c.idcarrera = i.idcarrera
-             WHERE a.idalumno = ? AND i.estado = 'Activo'`, [idAlumno]
-        );
-
-        // Verificar si se encontró la inscripción
-        if (inscripciones.length > 0) {
-            const inscripcion = inscripciones[0]; // Toma el primer resultado
-
-            // Define la ruta para guardar el PDF en la carpeta pública
-            const pdfDir = path.join(__dirname, 'public', 'pdfs');
-            if (!fs.existsSync(pdfDir)) fs.mkdirSync(pdfDir, { recursive: true });
-
-            const pdfPath = path.join(pdfDir, `ConstanciaInsc-N°_${inscripcion.idinscripcion}.pdf`);
-            
-            // Genera el PDF
-            await generarConstanciaPDF(inscripcion.idinscripcion, inscripcion.Carrera, inscripcion.fecha, 'Activo', inscripcion.Alumno, inscripcion.dni);
-
-            // Retorna la URL de descarga en la respuesta JSON
-            const downloadUrl = `/pdfs/ConstanciaInsc-N°${inscripcion.idinscripcion}.pdf`;
-            res.json({ success: true, downloadUrl });
-        } else {
-            res.status(404).json({ success: false, message: 'Inscripción no encontrada' });
-        }
-    } catch (error) {
-        console.error('Error al realizar la inscripción:', error);
-        res.status(500).json({
-            success: false,
-            message: 'Error al realizar la inscripción'
-        });
-    }
-});
-
-
-
-app.get('/descargar-pdf/:idInscripcion', (req, res) => {
-    const { idInscripcion } = req.params;
-    const pdfFilePath = path.join(__dirname, 'public', 'pdfs', `ConstanciaInsc-N°${idInscripcion}.pdf`);
-
-    // Verifica si el archivo existe antes de intentar descargarlo
-    if (!fs.existsSync(pdfFilePath)) {
-        return res.status(404).send('Archivo no encontrado');
-    }
-
-    // Envía el archivo para descargar y luego lo elimina
-    res.download(pdfFilePath, (err) => {
-        if (err) {
-            console.error('Error al descargar el archivo:', err);
-            res.status(500).send('Error al descargar el archivo');
-        } else {
-            // Elimina el archivo después de la descarga exitosa
-            fs.unlink(pdfFilePath, (unlinkErr) => {
-                if (unlinkErr) {
-                    console.error('Error al eliminar el archivo:', unlinkErr);
-                } else {
-                    console.log(`Archivo ${pdfFilePath} eliminado después de la descarga.`);
-                }
-            });
-        }
-    });
-});
-
-// ruta para obtener el id del alumno
-
-app.get('/preinscripcion', (req, res) => {
-    if (req.session.user) {
-        const idAlumno = req.session.user.idalumno;
-        res.render('preinscripcion', { idAlumno }); // Asegúrate de que esta vista se llama 'preinscripcion'
-    } else {
-        res.redirect('/login'); // Redirige al login si no está autenticado
-    }
-});
-/// GUARDAR LA INSCRIPCION DEL ALUMNO
 
 app.listen(port, '0.0.0.0',() => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
