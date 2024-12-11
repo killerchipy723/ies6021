edf94b56774be0e7f2f9363450b9ee2d239b3b0e
Diego Alderete <141648675+killerchipy723@users.noreply.github.com>
2024-12-10 21:55:06 -0300

    Prueba envio mail

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ app.post('/send-reset-email', async (req, res) => {
 
         // Configurar el mensaje para el correo electrónico
         const message = {
-            text: `Aquí está el enlace para restablecer su contraseña: ies6021jcdavalos.online/reset-password?token=${token}`,
+            text: `Aquí está el enlace para restablecer su contraseña:www.ies6021jcdavalos.online/reset-password?token=${token}`,
             from: 'ies6.021jc@gmail.com',
             to: email,
             subject: 'Restablecimiento de Contraseña'
